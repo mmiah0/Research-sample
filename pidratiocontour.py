33fb5a7bfb1cc106ca35c66ef07efd15c04dd234
Mamun Miah <mmiah0@gmail.com>
2020-04-15 16:10:02 -0700

    Update pidratiocontour.py

--- pidratiocontour.py
+++ pidratiocontour.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pylab import rcParams
+from matplotlib import ticker
 import scipy.interpolate
 from scipy import ndimage
 rcParams['figure.figsize'] = 25, 20
@@ -13,13 +14,13 @@ rcParams['font.sans-serif'] = "Arial"
 N = 1000 #number of points for plotting/interpolation
 x1, y1, z1 = np.genfromtxt(r'/Users/MMiah/Documents/mapping/gf_hypo_2019/fulldomain/analysis/pidratios/left_hypo_pid/40st_fn.txt', unpack=True)
 x2, y2, z2 = np.genfromtxt(r'/Users/MMiah/Documents/mapping/gf_hypo_2019/fulldomain/analysis/pidratios/center_hypo_pid/40st_fn.txt', unpack=True)
-#x3, y3, z3 = np.genfromtxt(r'/Users/MMiah/Documents/mapping/gf_hypo_2019/fulldomain/analysis/pidratios/right_hypo_pid/40st_fn.txt', unpack=True)
 
 print max(z1/z2)
 #Add to a dictionary the ratios of the PIDs and their locations in the domain
 dic={k:v for k,v in zip(zip(x1,y1),z1/z2)}
 print max(dic, key=dic.get) # Find maximum ratio in the domain
 #items = dic.items()
+
 # Declare two lists of elements containing the station indices for the subdomain
 l1=[i for i in range(9,92)]  # contains station index along horizontal
 l2=[i for i in range(11,21)]  # contains station index along normal
@@ -32,6 +33,7 @@ for i in range(len(x1)):
         ys.append(y1[i])
         zs.append(z1[i]/z2[i])
 print max(zs)
+
 #Add to a dictionary the ratios of the PIDs and their locations in the subdomain
 subdic={k:v for k,v in zip(zip(xs,ys),zs)}
 print max(subdic, key=subdic.get) # Find maximum ratio in the subdomain
@@ -46,8 +48,6 @@ smooth=[0,1,2,3,6]
 cmap = colors.ListedColormap(['blue','thistle','yellow','red'])
 bounds=smooth
 norm = colors.BoundaryNorm(bounds, cmap.N)
-#plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-#fig = plt.figure()
 im=plt.contourf(xi, yi, abs(zi), smooth, cmap=cmap, norm=norm, rotation=90)
 
 # uncomment the following to add space between tick labels and the axes
@@ -62,16 +62,8 @@ ax=plt.gca()
 ax.tick_params(axis='both', which='major', pad=18)
 #plt.colorbar()
 
-from matplotlib import ticker
-import matplotlib.colors as colors
-
-#cmap = colors.ListedColormap(['g','y','r'])
-bounds=smooth
 norm = colors.BoundaryNorm(bounds, cmap.N)
 cb = plt.colorbar(im, fraction=0.018, norm=norm, boundaries=bounds, ticks=smooth,)
-#tick_locator = ticker.MaxNLocator(nbins=4)
-#cb.locator = tick_locator
-#cb.update_ticks()
 cb.set_label('PID(L)/PID(C)', size=50, labelpad=40)
 cb.ax.tick_params(size=30,labelsize=40, pad = 15)
 #plt.savefig('140st_fnormal.pdf')
